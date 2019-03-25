aeb4a0bd7f6d72293858e267121e7fac38ffcf2c
Kenty Wang <kentymail@gmail.com>
2019-03-25 00:31:40 -0400

    Cleanup.

--- public/main.js
+++ public/main.js
@@ -1,16 +1,57 @@
-/*  Todo:
-    a11y
-    css
-      responsive
-      rem over px
-      sort
-    js
-    animation
-*/
+const app = new Vue({
+  el: '#app',
+  data: {
+    pea1: 'Yy',
+    pea2: 'Yy',
+  },
+  methods: {
+    setAlleles: function (event) {
 
-// const app = new Vue({
-//   el: '#app',
-//   data: {
-//     message: 'Hello Vue!'
-//   }
-// })
+      const id = event.path[2].id;
+      const value = event.target.value;
+
+      if (id === 'pea-1-genotype') {
+         this.pea1 = value;
+      } else if (id === 'pea-2-genotype') {
+        this.pea2 = value;
+      }
+    },
+    getImgFromGeno: genotype => (
+      genotype.includes('Y') ?
+      'images/yellow_pea.svg' :
+      'images/green_pea.svg'
+    ),
+  },
+  computed: {
+    cell1: function () {
+      return [this.pea2[0], this.pea1[0]].sort().join('');
+    },
+    cell2: function () {
+      return [this.pea2[0], this.pea1[1]].sort().join('');
+    },
+    cell3: function () {
+      return [this.pea2[1], this.pea1[0]].sort().join('');
+    },
+    cell4: function () {
+      return [this.pea2[1], this.pea1[1]].sort().join('');
+    },
+    cells: function () {
+      return [this.cell1, this.cell2, this.cell3, this.cell4];
+    },
+    count_YY: function () {
+      return this.cells.reduce((acc, curr) => acc += curr === 'YY', 0);
+    },
+    count_Yy: function () {
+      return this.cells.reduce((acc, curr) => acc += curr === 'Yy', 0);
+    },
+    count_yy: function () {
+      return this.cells.reduce((acc, curr) => acc += curr === 'yy', 0);
+    },
+    yellow_ratio: function () {
+      return this.cells.reduce((acc, curr) => acc += curr.includes('Y'), 0);
+    },
+    green_ratio: function () {
+      return this.cells.reduce((acc, curr) => acc += !curr.includes('Y'), 0);
+    },
+  }
+})
